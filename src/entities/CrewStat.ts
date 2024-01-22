fcc046b10b69e6fdd34598dd2746647a3a074fee
son <son@codegym.vn>
2024-01-22 14:14:55 +0700

    update

--- src/entities/CrewStat.ts
+++ src/entities/CrewStat.ts
@@ -1,12 +1,31 @@
+import {name} from "ts-jest/dist/transformers/hoist-jest";
+
 export default class CrewStat {
+
+    static HP(value: number) {
+        return new CrewStat("hp", value)
+    }
+
     private _name: string;
     private _value: number;
+
     private constructor(name: string, value: number) {
         this._name = name;
         this._value = value;
     }
+    get value(): number {
+        return this._value;
+    }
 
-    static HP(value: number) {
-        return new CrewStat("hp", value)
+    set value(value: number) {
+        this._value = value;
+    }
+
+    increase(amount: number) {
+        this.value += amount
+    }
+
+    get name(): string {
+        return this._name;
     }
 }
