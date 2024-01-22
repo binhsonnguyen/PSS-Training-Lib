9a1ddddc2229a1226cf33e5010cf04fe4452605f
son <son@codegym.vn>
2024-01-22 14:16:20 +0700

    update

--- src/entities/CrewStat.test.ts
+++ src/entities/CrewStat.test.ts
@@ -4,4 +4,13 @@ import CrewStat from "./CrewStat"
 test('test Crew Stat', () => {
     const crewHp = CrewStat.HP(3)
     expect(crewHp).not.toBeNull()
+    expect(crewHp.value).toBe(3)
+    crewHp.increase(2)
+    expect(crewHp.value).toBe(5)
+});
+
+test('test increase Crew Stat', () => {
+    const crewHp = CrewStat.HP(3)
+    crewHp.increase(2)
+    expect(crewHp.value).toBe(5)
 });
