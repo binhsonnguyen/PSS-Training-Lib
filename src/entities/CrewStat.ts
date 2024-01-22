a18b904cf5ce825815de69f8cd43503e76c5a016
son <son@codegym.vn>
2024-01-22 14:18:19 +0700

    update

--- src/entities/CrewStat.ts
+++ src/entities/CrewStat.ts
@@ -3,7 +3,31 @@ import {name} from "ts-jest/dist/transformers/hoist-jest";
 export default class CrewStat {
 
     static HP(value: number) {
-        return new CrewStat("hp", value)
+        return new CrewStat("HP", value)
+    }
+    static ATK(value: number) {
+        return new CrewStat("ATK", value)
+    }
+    static RPR(value: number) {
+        return new CrewStat("RPR", value)
+    }
+    static ABL(value: number) {
+        return new CrewStat("ABL", value)
+    }
+    static STA(value: number) {
+        return new CrewStat("STA", value)
+    }
+    static PLT(value: number) {
+        return new CrewStat("PLT", value)
+    }
+    static SCI(value: number) {
+        return new CrewStat("SCI", value)
+    }
+    static ENG(value: number) {
+        return new CrewStat("ENG", value)
+    }
+    static WPN(value: number) {
+        return new CrewStat("WPN", value)
     }
 
     private _name: string;
