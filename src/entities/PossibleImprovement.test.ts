c0356dc4cc030db0715ae61c1b4c0cd33a55c1df
son <son@codegym.vn>
2024-01-22 18:41:25 +0700

    update

--- src/entities/PossibleImprovement.test.ts
+++ src/entities/PossibleImprovement.test.ts
@@ -3,7 +3,7 @@ import PossibleImprovement from "./PossibleImprovement";
 
 test('test Crew Training Task', () => {
     const possibleImprovement = PossibleImprovement.HP().from(3).to(8)
-    expect(possibleImprovement.name).toBe("HP")
+    expect(possibleImprovement.name.toString()).toBe("HP")
     expect(possibleImprovement.minimumPossible).toBe(3)
     expect(possibleImprovement.maximumPossible).toBe(8)
 
