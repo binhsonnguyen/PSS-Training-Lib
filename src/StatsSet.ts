f5211250bb25a8ca22b642fabdc64766d758ef25
son <son@codegym.vn>
2024-01-23 10:55:10 +0700

    update

--- src/StatsSet.ts
+++ src/StatsSet.ts
@@ -3,17 +3,7 @@ import Stat from "./Stat";
 export default class StatsSet {
     private _stats = new Map<Stat, number>();
 
-    constructor(
-        hp: number = 0,
-        atk: number = 0,
-        rpr: number = 0,
-        abl: number = 0,
-        sta: number = 0,
-        plt: number = 0,
-        sci: number = 0,
-        eng: number = 0,
-        wpn: number = 0,
-    ) {
+    constructor(hp: number = 0, atk: number = 0, rpr: number = 0, abl: number = 0, sta: number = 0, plt: number = 0, sci: number = 0, eng: number = 0, wpn: number = 0,) {
         this._stats.set(Stat.HP, hp);
         this._stats.set(Stat.ATK, atk);
         this._stats.set(Stat.RPR, rpr);
@@ -23,6 +13,78 @@ export default class StatsSet {
         this._stats.set(Stat.SCI, sci);
         this._stats.set(Stat.ENG, eng);
         this._stats.set(Stat.WPN, wpn);
+    }
+
+    get hp() {
+        return this.get(Stat.HP)
+    }
+
+    set hp(value: number) {
+        this.set(Stat.HP, value)
+    }
+
+    get atk() {
+        return this.get(Stat.ATK)
+    }
+
+    set atk(value: number) {
+        this.set(Stat.ATK, value)
+    }
+
+    get rpr() {
+        return this.get(Stat.RPR)
+    }
+
+    set rpr(value: number) {
+        this.set(Stat.RPR, value)
+    }
+
+    get abl() {
+        return this.get(Stat.ABL)
+    }
+
+    set abl(value: number) {
+        this.set(Stat.ABL, value)
+    }
+
+    get sta() {
+        return this.get(Stat.STA)
+    }
+
+    set sta(value: number) {
+        this.set(Stat.STA, value)
+    }
+
+    get plt() {
+        return this.get(Stat.PLT)
+    }
+
+    set plt(value: number) {
+        this.set(Stat.PLT, value)
+    }
+
+    get sci() {
+        return this.get(Stat.SCI)
+    }
+
+    set sci(value: number) {
+        this.set(Stat.SCI, value)
+    }
+
+    get eng() {
+        return this.get(Stat.ENG)
+    }
+
+    set eng(value: number) {
+        this.set(Stat.ENG, value)
+    }
+
+    get wpn() {
+        return this.get(Stat.WPN)
+    }
+
+    set wpn(value: number) {
+        this.set(Stat.WPN, value)
     }
 
     get(stat: Stat): number {
