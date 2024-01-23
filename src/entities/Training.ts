de08b5247c94b5c28debfe70f0017d624a7a8d7d
son <son@codegym.vn>
2024-01-23 09:11:35 +0700

    update

--- src/entities/Training.ts
+++ src/entities/Training.ts
@@ -1,26 +1,18 @@
 import StatsSet from "./StatsSet";
 import TrainingTask from "./TrainingTask";
 import Stat from "./Stat";
-import StatGainMultiplier from "./StatGainMultiplier";
 import DecimalAdjust from "./DecimalAdjust";
 import Guaranteed from "./Guaranteed";
 
 export default class Training {
-    get currentTraining(): StatsSet {
-        return this._currentTraining;
+    constructor(totalTrainingPoint: number, fatigue: number, traingTask: TrainingTask, currentTraining: StatsSet,) {
+        this._totalTrainingPoint = totalTrainingPoint;
+        this._fatigue = fatigue;
+        this._traingTask = traingTask;
+        this._currentTraining = currentTraining;
     }
 
-    get traingTask(): TrainingTask {
-        return this._traingTask;
-    }
-
-    get fatigue(): number {
-        return this._fatigue;
-    }
-
-    set fatigue(value: number) {
-        this._fatigue = value;
-    }
+    private _totalTrainingPoint: number;
 
     get totalTrainingPoint(): number {
         return this._totalTrainingPoint;
@@ -30,27 +22,32 @@ export default class Training {
         this._totalTrainingPoint = value;
     }
 
-    private _totalTrainingPoint: number;
     private _fatigue: number;
+
+    get fatigue(): number {
+        return this._fatigue;
+    }
+
+    set fatigue(value: number) {
+        this._fatigue = value;
+    }
+
     private _traingTask: TrainingTask;
+
+    get traingTask(): TrainingTask {
+        return this._traingTask;
+    }
+
     private _currentTraining: StatsSet;
 
-    constructor(
-        totalTrainingPoint: number,
-        fatigue: number,
-        traingTask: TrainingTask,
-        currentTraining: StatsSet,
-    ) {
-        this._totalTrainingPoint = totalTrainingPoint;
-        this._fatigue = fatigue;
-        this._traingTask = traingTask;
-        this._currentTraining = currentTraining;
+    get currentTraining(): StatsSet {
+        return this._currentTraining;
     }
 
     requiredConsumerableFactor(stat: Stat) {
         let factor1 = 1 - this.currentTraining.total() / this.totalTrainingPoint
         let factor2 = 1 - this.currentTraining.get(stat) / this.totalTrainingPoint
-        let factor3 = StatGainMultiplier.forFatigue(this.fatigue)
+        let factor3 = this.statGainMultiplier()
         let factor = 1 / (factor1 * factor2 * factor3)
         return DecimalAdjust.floor(factor, -4)
     }
@@ -58,14 +55,11 @@ export default class Training {
     maximumPossibleImprovement(stat: Stat) {
         if (this.traingTask.isMainStat(stat) && this.fatigue100()) {
             return Guaranteed.forQuality(this.traingTask.quality);
-        } else if (this.fatigue1to50()
-            && this.traingTask.trainingEffect(stat)
-            / this.requiredConsumerableFactor(stat)
-            > 1.5) {
+        } else if (this.fatigue1to50() && this.traingTask.trainingEffect(stat) / this.requiredConsumerableFactor(stat) > 1.5) {
             return this.statGainFactor(stat)
         } else {
             const factor = this.statGainFactor(stat);
-            const multiplier = StatGainMultiplier.forFatigue(this.fatigue);
+            const multiplier = this.statGainMultiplier();
             const statGain = this.statGain(stat, factor, multiplier);
             if (this.traingTask.isMainStat(stat) && this.traingTask.quality.alwaysGainningStat()) {
                 const guaranted = Guaranteed.forQuality(this.traingTask.quality);
@@ -80,7 +74,7 @@ export default class Training {
         }
     }
 
-    statGain(stat: Stat, factor: number, multiplier: number) {
+    private statGain(stat: Stat, factor: number, multiplier: number) {
         if (stat == Stat.STA) {
             return DecimalAdjust.ceil(factor * multiplier, 0);
         } else {
@@ -88,11 +82,23 @@ export default class Training {
         }
     }
 
+    private statGainMultiplier(): number {
+        if (this.noFatigue()) {
+            return 1;
+        } else if (this.fatigue1to50()) {
+            return 0.5
+        } else if (this.fatigue51to99()) {
+            return 1 / 3;
+        } else {
+            return 0
+        }
+    }
+
     private statGainFactor(stat: Stat) {
         const factor1 = this.traingTask.trainingEffect(stat)
         const factor2 = 1 - this.currentTraining.total() / this.totalTrainingPoint
         const factor3 = 1 - this.currentTraining.get(stat) / this.totalTrainingPoint
-        return  factor1 * factor2 * factor3
+        return factor1 * factor2 * factor3
     }
 
     private noFatigue() {
@@ -109,10 +115,6 @@ export default class Training {
 
     private fatigue100() {
         return this.fatigue == 100
-    }
-
-    private maximumEffectivenessFactor(stat: Stat) {
-
     }
 
 }
