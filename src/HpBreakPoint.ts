04277e08a858d7d96fb56436af9376ef4b3ad594
son <son@codegym.vn>
2024-05-23 16:01:22 +0700

    passs

--- src/HpBreakPoint.ts
+++ src/HpBreakPoint.ts
@@ -3,23 +3,23 @@ import Decimal from "decimal.js";
 export class HpBreakPoint {
     private _baseHp: Decimal
     private _hpAddition: Decimal
-    private constructor(base: number, hpAddition = 0) {
+    private constructor(base: number, hpAddition = 1) {
         this._baseHp = new Decimal(base)
         this._hpAddition = new Decimal(hpAddition)
     }
 
-    static base(hp: number) {
+    static withBase(hp: number) {
         return new HpBreakPoint(hp)
     }
 
-    additionHp(hp: number) {
+    withAdditionHp(hp: number) {
         return new HpBreakPoint(this._baseHp.toNumber(), hp)
     }
 
     getValue() {
         let additionTp = new Decimal(0)
         let additionHp = new Decimal(0)
-        let toPassBreakpoint: Decimal = new Decimal(0.5)
+        let toPassBreakpoint: Decimal = this._hpAddition.sub(1).plus(0.5)
         do {
             additionTp = additionTp.plus(1)
             const multiply = additionTp.dividedBy(100)
--- src/HpBreakpoint.test.ts
+++ src/HpBreakpoint.test.ts
@@ -2,23 +2,24 @@ import {expect, test} from "@jest/globals";
 import {HpBreakPoint} from "./HpBreakPoint";
 
 test('test first hp breakpoint', () => {
-    expect(HpBreakPoint.base(1).getValue()).toBe(50)
-    expect(HpBreakPoint.base(2).getValue()).toBe(25)
-    expect(HpBreakPoint.base(3).getValue()).toBe(17)
-    expect(HpBreakPoint.base(4).getValue()).toBe(13)
-    expect(HpBreakPoint.base(5).getValue()).toBe(10)
-    expect(HpBreakPoint.base(6).getValue()).toBe(9)
-    expect(HpBreakPoint.base(7).getValue()).toBe(8)
-    expect(HpBreakPoint.base(8).getValue()).toBe(7)
-    expect(HpBreakPoint.base(9).getValue()).toBe(6)
-    expect(HpBreakPoint.base(10).getValue()).toBe(5)
-    expect(HpBreakPoint.base(11).getValue()).toBe(5)
-    expect(HpBreakPoint.base(12).getValue()).toBe(5)
-    expect(HpBreakPoint.base(13).getValue()).toBe(4)
-    expect(HpBreakPoint.base(14).getValue()).toBe(4)
+    expect(HpBreakPoint.withBase(1).getValue()).toBe(50)
+    expect(HpBreakPoint.withBase(2).getValue()).toBe(25)
+    expect(HpBreakPoint.withBase(3).getValue()).toBe(17)
+    expect(HpBreakPoint.withBase(4).getValue()).toBe(13)
+    expect(HpBreakPoint.withBase(5).getValue()).toBe(10)
+    expect(HpBreakPoint.withBase(6).getValue()).toBe(9)
+    expect(HpBreakPoint.withBase(7).getValue()).toBe(8)
+    expect(HpBreakPoint.withBase(8).getValue()).toBe(7)
+    expect(HpBreakPoint.withBase(9).getValue()).toBe(6)
+    expect(HpBreakPoint.withBase(10).getValue()).toBe(5)
+    expect(HpBreakPoint.withBase(11).getValue()).toBe(5)
+    expect(HpBreakPoint.withBase(12).getValue()).toBe(5)
+    expect(HpBreakPoint.withBase(13).getValue()).toBe(4)
+    expect(HpBreakPoint.withBase(14).getValue()).toBe(4)
 });
 
 test('test continuos hp breakpoint', () => {
-    expect(HpBreakPoint.base(10).getValue()).toBe(5)
-    expect(HpBreakPoint.base(10).additionHp(1).getValue()).toBe(5)
+    expect(HpBreakPoint.withBase(10).getValue()).toBe(5)
+    expect(HpBreakPoint.withBase(10).withAdditionHp(1).getValue()).toBe(5)
+    expect(HpBreakPoint.withBase(10).withAdditionHp(2).getValue()).toBe(15)
 });
